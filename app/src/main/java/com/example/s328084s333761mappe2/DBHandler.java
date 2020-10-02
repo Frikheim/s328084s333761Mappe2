fbc49621bb404176e32002498b226a525d313d6b
Frikheim <fredrik.rikheim@outlook.com>
2020-10-02 13:03:00 +0200

    Fikset foreign key i møtedeltakelse og laget leggtil funksjoner for møte og møteDeltakelse

--- app/src/main/java/com/example/s328084s333761mappe2/DBHandler.java
+++ app/src/main/java/com/example/s328084s333761mappe2/DBHandler.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class DBHandler extends SQLiteOpenHelper {
     static String TABLE_KONTAKTER = "Kontakter";
     static String TABLE_MØTER = "Møter";
-    static String TABLE_MØTEDELTAKELSE = "MøteDeltakelse";
+    static String TABLE_MØTE_DELTAKELSE = "MøteDeltakelse";
     static String KEY_ID_KONTAKT = "_ID";
     static String KEY_NAME = "Navn";
     static String KEY_PH_NO = "Telefon";
@@ -22,8 +22,8 @@ public class DBHandler extends SQLiteOpenHelper {
     static String KEY_STED = "Sted";
     static String KEY_TID = "Tidspunkt";
     static String KEY_ID_MØTEDELTAKELSE = "_ID";
-    static String KEY_MØTE_ID = "_ID";
-    static String KEY_DELTAKER_ID = "Deltaker_ID";
+    static String KEY_ID_DELTAKER = "_ID";
+    static String KEY_ID_MØTE_ID = "_ID";
     static int DATABASE_VERSION = 1;
     static String DATABASE_NAME = "MøteDatabase";
     public DBHandler(Context context) {
@@ -42,18 +42,37 @@ public class DBHandler extends SQLiteOpenHelper {
                 " TEXT," + KEY_TID + " TEXT" + ")";
         Log.d("SQL", LAG_TABELL_MØTER);
         db.execSQL(LAG_TABELL_MØTER);
-        String LAG_TABELL_MØTEDELTAKELSE = "CREATE TABLE " + TABLE_MØTEDELTAKELSE + "(" + KEY_ID_MØTEDELTAKELSE +
-                " INTEGER PRIMARY KEY," + KEY_MØTE_ID + " FOREIGN KEY REFERENCES "+TABLE_MØTER+"("+KEY_MØTE_ID+"), "
-                + KEY_ID_KONTAKT +" FOREIGN KEY REFERENCES "+TABLE_KONTAKTER+"("+KEY_ID_KONTAKT+"));";
+        String LAG_TABELL_MØTEDELTAKELSE = "CREATE TABLE " + TABLE_MØTE_DELTAKELSE + "(" + KEY_ID_MØTEDELTAKELSE +
+                " INTEGER PRIMARY KEY, " + KEY_ID_MØTE_ID + "INTEGER, FOREIGN KEY (" + KEY_ID_MØTE_ID + ") REFERENCES "+TABLE_MØTER+"("+KEY_ID_MØTE+"), "
+                + KEY_ID_DELTAKER +" FOREIGN KEY (" + KEY_ID_DELTAKER + ") REFERENCES "+TABLE_KONTAKTER+"("+KEY_ID_KONTAKT+"));";
 
-        Log.d("SQL", LAG_TABELL_MØTER);
-        db.execSQL(LAG_TABELL_MØTER);
+        Log.d("SQL", LAG_TABELL_MØTEDELTAKELSE);
+        db.execSQL(LAG_TABELL_MØTEDELTAKELSE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_KONTAKTER);
         onCreate(db);
+    }
+
+    public void leggTilMøte (Møte møte) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(KEY_TYPE, møte.getType());
+        values.put(KEY_STED, møte.getSted());
+        values.put(KEY_TID, møte.getTidspunkt());
+        db.insert(TABLE_MØTER,null,values);
+        db.close();
+    }
+
+    public void leggTilMøteDeltakelse (MøteDeltakelse møteDeltakelse) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(KEY_ID_MØTE, møteDeltakelse.getMøte_ID());
+        values.put(KEY_ID_KONTAKT, møteDeltakelse.getDeltaker_ID());
+        db.insert(TABLE_MØTER,null,values);
+        db.close();
     }
 
     public void leggTilKontakt(Kontakt kontakt) {
@@ -83,6 +102,28 @@ public class DBHandler extends SQLiteOpenHelper {
         }
         return kontaktListe;
     }
+
+    public List<Møte> finnAlleMøter() {
+        List<Møte> møteListe = new ArrayList<Møte>();
+        String selectQuery = "SELECT * FROM " + TABLE_MØTER;
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery,null);
+        if (cursor.moveToFirst()) {
+            do {
+                Møte møte = new Møte();
+                møte.set_ID(cursor.getLong(0));
+                møte.setType(cursor.getString(1));
+                møte.setSted(cursor.getString(2));
+                møte.setTidspunkt(cursor.getString(3));
+                møteListe.add(møte);
+            } while (cursor.moveToNext());
+            cursor.close();
+            db.close();
+        }
+        return møteListe;
+    }
+
+
 
     public void slettKontakt(Long inn_id) {
         SQLiteDatabase db = this.getWritableDatabase();
